7a6b3574301634226f3bdf3241fcc7b1ffe51267
marmooo <marmooo@users.noreply.github.com>
2024-01-14 09:24:26 +0900

    bump eta from 3.1.1 to 3.2.0

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
-import { Eta } from "https://deno.land/x/eta@v3.1.1/src/index.ts";
+import { Eta } from "https://deno.land/x/eta@v3.2.0/src/index.ts";
 import { JKAT, Kanji } from "npm:@marmooo/kanji@0.0.2";
 
 const dirNames = [
